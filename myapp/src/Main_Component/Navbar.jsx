ca9f53a6ecce02a2c0cae7a32a7b15087dbcd1dc
Roop Teja <roopteja112@gmail.com>
2025-05-13 07:14:28 +0530

    bug fixes in homepage

--- myapp/src/Main_Component/Navbar.jsx
+++ myapp/src/Main_Component/Navbar.jsx
@@ -2,22 +2,20 @@ import React from 'react'
 import SunnyIcon from '@mui/icons-material/Sunny';
 import BedtimeIcon from '@mui/icons-material/Bedtime';
 import LogoutIcon from '@mui/icons-material/Logout';
-import { useNavigate } from 'react-router-dom';
 
 const Navbar = ({ handleTheme, theme }) => {
 
     const username = localStorage.getItem("userName");
-    const Navigate = useNavigate();
 
     const handleLogout = (e) => {
         e.preventDefault();
         localStorage.removeItem("Login")
-        Navigate("/")
+        window.location.href = "/"
     }
 
     return (
         <>
-            <div className='bg-green-800 text-white relative w-[90%] h-14 top-10 mx-18 rounded-4xl flex flex-row items-center px-10 justify-between'>
+            <div className='bg-green-700 text-white relative w-[92%] h-14 top-10 mx-16 rounded-4xl flex flex-row items-center px-10 justify-between'>
                 <span className='text-xl font-semibold tracking-wide'>Make My Trip</span>
                 <div className='flex space-x-15'>
                     <span className='cursor-pointer'>Home</span>
